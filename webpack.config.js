f54012fc408ebf5432ec0a63d4c268584fe60d79
bimonestle <bimonestle@yahoo.co.id>
2021-03-14 21:30:57 +0700

    Define rules for webpack loader. 'test', 'exclude', 'use'

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,20 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'deploy'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack Output',
